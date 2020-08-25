536eed82a4c0e3169710fb91ca305b7e4ba2cc33
Andree Yosua <andree.yosua@gmail.com>
2020-08-25 09:25:40 +0700

    Add resize using applyOptions input

--- src/charts-button/index.ts
+++ src/charts-button/index.ts
@@ -58,19 +58,42 @@ forwardButton.addEventListener("click", (ev) => {
 const chartViewport = chartContainer.querySelector("table > tr > td:nth-child(2) > div") as HTMLDivElement;
 chartViewport.append(forwardButton);
 
+const useApplyOptions = document.getElementById("apply-options-check") as HTMLInputElement;
+
 const size1 = document.getElementById("size-1") as HTMLButtonElement;
 size1.addEventListener("click", (ev) => {
-    chart.resize(400, 300);
+    if (useApplyOptions.checked) {
+        chart.applyOptions({
+            width: 400,
+            height: 300
+        });
+    } else {
+        chart.resize(400, 300);
+    }
 });
 
 const size2 = document.getElementById("size-2") as HTMLButtonElement;
 size2.addEventListener("click", (ev) => {
-    chart.resize(800, 600);
+    if (useApplyOptions.checked) {
+        chart.applyOptions({
+            width: 800,
+            height: 600
+        });
+    } else {
+        chart.resize(800, 600);
+    }
 });
 
 const size3 = document.getElementById("size-3") as HTMLButtonElement;
 size3.addEventListener("click", (ev) => {
-    chart.resize(1024, 768);
+    if (useApplyOptions.checked) {
+        chart.applyOptions({
+            width: 1024,
+            height: 768
+        });
+    } else {
+        chart.resize(1024, 768);
+    }
 });
 
 const precision1 = document.getElementById("precision-1") as HTMLButtonElement;
